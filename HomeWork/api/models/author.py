423364290f45300d6f50b4ba9a459d8050798020
AlexeyFadin <alexeyfadin75@gmail.com>
2025-12-04 17:44:54 +0300

    Add models to autors field  Surname

--- HomeWork/api/models/author.py
+++ HomeWork/api/models/author.py
@@ -8,10 +8,12 @@ class AuthorModel(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(32), index=True, unique=True)
+    surname: Mapped[str] = mapped_column(String(32), index=True, unique=True)
     quotes: Mapped[list['QuoteModel']] = relationship(back_populates='author', lazy='dynamic')
 
-    def __init__(self, name):
+    def __init__(self, name, surname):
         self.name = name
+        self.surname = surname
 
     def to_dict(self):
-        return {"id": self.id, "name": self.name}
+        return {"id": self.id, "name": self.name, "surname": self.surname}
